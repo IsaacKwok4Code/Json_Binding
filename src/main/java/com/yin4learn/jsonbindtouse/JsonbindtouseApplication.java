271a86e275242d0fd1024e1ebc950b42c4e49e63
IsaacKwok(Yin) <58442187+IsaacKwok4Code@users.noreply.github.com>
2021-08-01 13:31:44 -0400

    update code

---
+++ src/main/java/com/yin4learn/jsonbindtouse/JsonbindtouseApplication.java
@@ -0,0 +1,13 @@
+package com.yin4learn.jsonbindtouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonbindtouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonbindtouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yin4learn/jsonbindtouse/JsonbindtouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yin4learn.jsonbindtouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonbindtouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
